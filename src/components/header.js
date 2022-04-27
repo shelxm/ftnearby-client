a339a79f48fd7b464450db2d69942bb6d852f9ac
Sheldon <81538781+shelxm@users.noreply.github.com>
2022-04-27 16:42:33 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { NavDropdown } from "bootstrap";
+//import { NavDropdown } from "bootstrap";
 import authService from "./redux/authService";
 
 const Header = () => {
