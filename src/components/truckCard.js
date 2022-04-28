3bb4de79a90a978d9dd1c42361e62dbe01bcb6db
Sheldon <81538781+shelxm@users.noreply.github.com>
2022-04-27 21:03:51 -0500

    Update truckCard.js

--- src/components/truckCard.js
+++ src/components/truckCard.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const TruckCard = ({foodtruck}) => {
-    //console.log(foodtruck);
-    return (
-        <div className="card">
-            <img src="{foodtruck.imageUrl}" className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">{foodtruck.name}</h5>
-                <p className="card-text">{foodtruck.hours}</p>
-                <p className="card-text">{foodtruck.location}</p>
-                <a href={foodtruck.link} className="btn btn-primary">For more information</a>
-            </div>
-        </div>
-    )
+const TruckCard = ({ foodtruck }) => {
+  //console.log(foodtruck);
+  return (
+    <div className="card container">
+      <img src="{foodtruck.imageUrl}" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{foodtruck.name}</h5>
+        <p className="card-text">{foodtruck.hours}</p>
+        <p className="card-text">{foodtruck.location}</p>
+        <a href={foodtruck.link} className="btn btn-primary">
+          For more information
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default TruckCard;
