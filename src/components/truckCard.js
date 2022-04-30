d99960a1675dd8512808cc54f0cab4f2d3747ded
Sheldon <81538781+shelxm@users.noreply.github.com>
2022-04-29 22:42:17 -0500

    Update truckCard.js

--- src/components/truckCard.js
+++ src/components/truckCard.js
@@ -13,7 +13,7 @@ const TruckCard = ({ foodtruck }) => {
         <a href={foodtruck.link} className="btn btn-primary">
           For more information
         </a>
-        <>{FavoriteButton}</>
+        <span>{FavoriteButton}</span>
       </div>
     </div>
   );
