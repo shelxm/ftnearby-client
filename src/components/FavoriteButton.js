b1d3ec214ebd84219e2d3f47204e7681af2d3418
Sheldon <81538781+shelxm@users.noreply.github.com>
2022-04-30 19:41:16 -0500

    fixes

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import API from './redux/api';
 
 const FavoriteButton = () => {
-    const [isFavorite, setIsFavorite] = useState([]);
+    const [isFavorite, setIsFavorite] = useState();
 
     /*useEffect(() => {
         const user = JSON.parse(window.localStorage.getItem("user"));
@@ -20,42 +20,42 @@ const FavoriteButton = () => {
           .catch((err) => {
             console.log("Error from favorites.");
           });
-    }, []);
+    }, []);*/
 
     function handleOnClick(event) {
         if (isFavorite) {
-            axios.delete(API + "user/favorites/" + event.target.truckId)
+            isFavorite=false
         }
-        axios.post(API + "user/favorites/", { id: event.target.truckId})
+        setIsFavorite(event.target)
     }
     //user favorites array
     //if truckId in user.favorites
     //isFav
 
-    /*
-    function onToggle(e) {
+    
+   /* function onToggle(e) {
         if (isFavorite) {
 
         }
-    }
-    */
+    }*/
+    
 
     return (
-        /*<>
-            {isFavorited ? (
-                <button className="btn">
+        <>
+            {isFavorite ? (
+                <button className="btn" onClick={handleOnClick}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                     </svg>
                 </button>
-            ) : (*/
-                <button className="btn">
+            ) : (
+                <button className="btn" onClick={handleOnClick}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                         <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
                     </svg>
                 </button>
-            //)}
-        //</>
+            )}
+        </>
     );
 };
 
