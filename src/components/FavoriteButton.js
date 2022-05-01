b9abe4e757285685f0daf9a8ad21a06ef206e24a
Sheldon <81538781+shelxm@users.noreply.github.com>
2022-04-30 19:21:00 -0500

    Update FavoriteButton.js

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import API from './redux/api';
 
@@ -42,7 +42,7 @@ const FavoriteButton = () => {
 
     return (
         <>
-            {isFavorited ? (
+            {isFavorite ? (
                 <button className="btn" onClick={handleOnClick}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
